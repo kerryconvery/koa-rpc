6c6ab0acb3efca05ccaf42695391722cc7b06e24
Kerry Convery <kerry.convery@gmail.com>
2024-09-13 22:31:46 +1000

    improve console log messages

--- src/rpc-client.ts
+++ src/rpc-client.ts
@@ -10,10 +10,10 @@ export type ClientSdk = typeof clientSdk
 
 clientSdk.echo({ value: 'hello' }).then((result) => console.log('echo:', result.data))
 clientSdk.split({ value: 'q,w,e,r,t,y', delimiter: ',' }).then((result) => console.log('split:', result.data))
-clientSdk.getCandidate({ candidateId: 1245678 }).then((result: Result<CandidateOutput>) => console.log('candidate', result.data))
+clientSdk.getCandidate({ candidateId: 1245678 }).then((result: Result<CandidateOutput>) => console.log('get candidate result:', result.data))
 clientSdk.updateCandidate({
   candidateId: 12456,
   firstName: 'John',
   lastName: 'Jones',
   emailAddress: 'jj@gg.com'
-}).then((result: Result<void>) => console.log(result))
+}).then((result: Result<void>) => console.log('update candidate result:', result))
